a15be6a65ee5a0628fd42bb4846624401860cc12
progRanu <frontenddevrg22@gmail.com>
2025-07-29 22:25:06 +0530

    Interview Question on Equality Check In JS

---
+++ equalityCheck.js
@@ -0,0 +1,16 @@
+let a = "2" + "2"; 
+let b = "22"; 
+console.log(a === b);//true
+console.log(typeof NaN)//number
+console.log(Number.isNaN())//false
+console.log(NaN ===NaN)//false
+console.log(NaN == NaN)//false
+console.log(typeof null)//object
+console.log(null == 0)//false
+console.log(null == [])//false
+console.log(null == {})//false
+console.log(null == false)//false
+console.log(null == "")//false
+console.log(null === null)//true
+console.log(null == undefined)//true
+console.log(null === undefined)//false
